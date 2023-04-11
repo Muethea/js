2c232c2bd0fab4f158e4aec20fc504a2d2636bb5
Fernando dos Santos Muethea <fmuethea4@gmail.com>
2023-04-11 13:39:14 +0200

    operacoes js

---
+++ operacoes/index.js
@@ -0,0 +1,19 @@
+// let n = 25
+// let n2 = 5
+
+// const soma = n + n2
+// const sub = n - n2
+// const mult = n * n2
+// const div = n / n2
+
+// const mod = n % n2
+
+// console.log(
+//   `soma = ${soma}, sub = ${sub} a mult = ${mult} e a divisao = ${div} modo = ${mod}`
+// )
+
+const nome = 'Fernando dos Santos Muethea'
+
+
+
+console.log(nome.)
